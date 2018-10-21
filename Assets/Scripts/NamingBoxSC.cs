06d8126e420743b01fd0f4641906c01e4da9d9fd
SerenityWhite <44226782+SerenityWhite@users.noreply.github.com>
2018-10-21 18:19:31 +0900

    1021 집에서
    집에서 작업해놓은 것들 패치

---
+++ Assets/Scripts/NamingBoxSC.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NamingBoxSC : MonoBehaviour
+{
+    public UIInput NamingInput;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public void NameSetClick()
+    {
+        PlayerPrefs.SetString("UserName", NamingInput.value);
+        Application.LoadLevel(1);
+    }
+}
---
+++ Assets/Scripts/StartSC.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSC : MonoBehaviour
+{
+    public GameObject NamingBox;
+
+	void Start ()
+    {
+		
+	}
+
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            NamingBox.SetActive(true);
+            gameObject.SetActive(false);
+        }
+	}
+}
